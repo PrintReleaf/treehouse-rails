4d0c3ed7555641b04faf03684b449d182c49c7ae
Casey O'Hara <casey.ohara@me.com>
2018-04-11 12:56:32 -0600

    Use Rails 5-compatible cookie decryption

--- lib/treehouse/session.rb
+++ lib/treehouse/session.rb
@@ -17,8 +17,8 @@ module Treehouse
       @cookie ||= decrypted_cookie
     end
 
-    def cookies
-      @cookies ||= ActionDispatch::Request.new(@env).cookie_jar
+    def cookie_jar
+      @cookie_jar ||= ActionDispatch::Request.new(@env).cookie_jar
     end
 
     def find_current_login
@@ -40,12 +40,16 @@ module Treehouse
     def decrypted_cookie
       secret_key = Treehouse.key
       cookie_key = Treehouse.cookie
-      cookie = ActionDispatch::Cookies::EncryptedCookieJar.new(
-        { cookie_key => cookies[cookie_key] },
-        ActiveSupport::KeyGenerator.new(secret_key, iterations: 1000),
-        { encrypted_cookie_salt: 'encrypted cookie', encrypted_signed_cookie_salt: 'signed encrypted cookie', serializer: :json }
-      )[cookie_key]
-      return cookie || {}
+      cookie_value = cookie_jar[cookie_key]
+      return {} if cookie_value.blank?
+      cookie_value = CGI::unescape(cookie_value)
+      salt = 'encrypted cookie'
+      signed_salt = 'signed encrypted cookie'
+      key_generator = ActiveSupport::KeyGenerator.new(secret_key, iterations: 1000)
+      secret = key_generator.generate_key(salt)[0, 32]
+      signed_secret = key_generator.generate_key(signed_salt)
+      encryptor = ActiveSupport::MessageEncryptor.new(secret, signed_secret, serializer: JSON)
+      return encryptor.decrypt_and_verify(cookie_value) || {}
     end
   end
 end
